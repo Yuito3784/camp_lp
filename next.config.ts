37070a17a47c550bcedfd8973c3fd1446974d092
Yuito Ohara <yuito@example.com>
2025-10-30 16:04:51 +0900

    chore: setup GitHub Pages via Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const isGH = process.env.GITHUB_PAGES === "true";
+const repo = "camp_lp";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: isGH ? `/${repo}` : "",
+  assetPrefix: isGH ? `/${repo}/` : "",
+  trailingSlash: true,
 };
-
 export default nextConfig;
